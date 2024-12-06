18e4644718430a3640dd04fdebd2471063cb21be
Panda <Panda@China.q>
2024-12-06 11:29:42 +0300

    добавил home

---
+++ precache-manifest.b011078878927abc95e88d716bb5c091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a743b73ea537cd98e9ff230147b25ebc",
+    "url": "/cookbook/index.html"
+  },
+  {
+    "revision": "7d74408c0a8581e5124b",
+    "url": "/cookbook/static/css/main.110449b6.chunk.css"
+  },
+  {
+    "revision": "c08dffa73d1313ba37e4",
+    "url": "/cookbook/static/js/2.b376b0bf.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/cookbook/static/js/2.b376b0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d74408c0a8581e5124b",
+    "url": "/cookbook/static/js/main.10a17302.chunk.js"
+  },
+  {
+    "revision": "104a9c1ba58de01bf1ba",
+    "url": "/cookbook/static/js/runtime-main.18deefa4.js"
+  },
+  {
+    "revision": "e2ab12b59f853635c67a4d5b1fe88452",
+    "url": "/cookbook/static/media/cook.e2ab12b5.png"
+  }
+]);
